c1dde2e30a7620f9893a032150a45fcac889eab0
CarsonLeavitt <cdl016@uark.edu>
2023-10-14 14:04:09 -0500

    Define run_query function

--- Model/queries.py
+++ Model/queries.py
@@ -5,6 +5,18 @@
     # Currently, returns results as a pandas dataframe
     # Will need to be updated to return results as object(s) of a class (Hero, Power, etc.)
 
+
 import sqlite3
 import pandas as pd
 from pathlib import Path
+
+
+# Define the path to the database
+db_file = Path.cwd() / 'database.db'
+
+
+# Define basic function to return a database and return a dataframe
+# Using the with keyword to ensure the connection is closed after query is executed
+def run_query(db_file, query_text='', params=None):
+    with sqlite3.connect(db_file) as cnn:
+        return pd.read_sql(query_text, cnn, params=params)
