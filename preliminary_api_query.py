adbd296ad92a17d2f14817888006ef4123788965
CarsonLeavitt <cdl016@uark.edu>
2023-10-31 20:20:22 -0500

    basic function to run a query

--- preliminary_api_query.py
+++ preliminary_api_query.py
@@ -1,1 +1,13 @@
-# Preliminary file to test a one query and API function in a single file
+# Preliminary file to test one query and API function in a single file
+
+import sqlite3
+import pandas as pd
+from pathlib import Path
+
+# basic function to run a query and return a dataframe, closing the connection when done
+def run_query(query, db_file, params=None):
+    with sqlite3.connect(db_file) as cnn:
+        return pd.read_sql(query, cnn, params=params)
+    
+
+
