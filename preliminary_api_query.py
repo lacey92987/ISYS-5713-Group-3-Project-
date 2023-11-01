17e8f622ee1dca7e2a99db14bfa3b411383346a6
CarsonLeavitt <cdl016@uark.edu>
2023-10-31 20:44:32 -0500

    query to get power by id

--- preliminary_api_query.py
+++ preliminary_api_query.py
@@ -4,16 +4,25 @@ import sqlite3
 import pandas as pd
 from pathlib import Path
 
+# specify the database file
+data_folder = Path("Model")
+db_file = data_folder / "database.db"
+
+
 # basic function to run a query and return a dataframe, closing the connection when done
 def run_query(query, db_file, params=None):
     with sqlite3.connect(db_file) as cnn:
         return pd.read_sql(query, cnn, params=params)
     
-# specify the database file
-data_folder = Path("Model")
-db_file = data_folder / "database.db"
 
-# specify a simple query to test the database connection
-query ="""SELECT * FROM heroes LIMIT 10;"""
-results = run_query(query, db_file)
-print(results)
+# define a query to get a power by id
+def get_power(id):
+    query = "SELECT * FROM powers WHERE power_id = ?;"
+    params = (id,)
+    results = run_query(query, db_file, params)
+    return results
+
+# test
+id = 10
+test_result= get_power(id)
+print(test_result)
