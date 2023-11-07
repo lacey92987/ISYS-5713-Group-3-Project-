f4a5e018cb7d23f53afe04a16c824c6e9f149bda
lacey92987 <73759421+lacey92987@users.noreply.github.com>
2023-11-07 09:40:52 -0600

    created code to compare two heroes and their power levels

--- Model/api.py
+++ Model/api.py
@@ -405,6 +405,56 @@ def remove_hero(id):
     conn.commit()
     return {'success':True}
 
+#GET  function to compare two heroes power levels
+@app.route('/compare_power', methods=['GET'])
+def compare_power():
+    hero_id1 = request.args.get('hero_id1')
+    hero_id2 = request.args.get('hero_id2')
+
+    if not hero_id1 or not hero_id2:
+        return jsonify({"error": "Both hero_id1 and hero_id2 parameters are required"}), 400
+
+    conn = sqlite3.connect(DATABASE_FILE)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT h1.hero_name AS hero_name1, SUM(p1.power_level) AS total_power1,
+           h2.hero_name AS hero_name2, SUM(p2.power_level) AS total_power2
+        FROM heroes AS h1
+        JOIN heroes_powers AS hp1 ON h1.hero_id = hp1.hero_id
+        JOIN powers AS p1 ON hp1.power_id = p1.power_id
+        JOIN heroes AS h2 ON h2.hero_id = ?
+        JOIN heroes_powers AS hp2 ON h2.hero_id = hp2.hero_id
+        JOIN powers AS p2 ON hp2.power_id = p2.power_id
+        WHERE h1.hero_id = ? AND h2.hero_id = ?
+    """, (hero_id2, hero_id1, hero_id2))
+
+    result = cursor.fetchone()
+    conn.close()
+
+    if result is None:
+        return jsonify({"error": "One or both heroes not found"}), 404
+
+    hero_name1, total_power1, hero_name2, total_power2 = result
+    if total_power1 > total_power2:
+        winner = hero_name1
+    elif total_power1 < total_power2:
+        winner = hero_name2
+    else:
+        winner = "Tie"
+
+    response = {
+        "hero_id1": hero_id1,
+        "hero_name1": hero_name1,
+        "total_power1": total_power1,
+        "hero_id2": hero_id2,
+        "hero_name2": hero_name2,
+        "total_power2": total_power2,
+        "winner": winner,
+    }
+
+    return jsonify(response)
+    
 ###
 # Main
 ###
