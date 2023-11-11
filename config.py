0d7365eb58e2fc4130a89fc8b1add1cb2aada03d
CarsonLeavitt <cdl016@uark.edu>
2023-11-11 11:36:44 -0600

    Raise exceptions in create_database_schema() and load_data()

--- config.py
+++ config.py
@@ -8,100 +8,114 @@ from controller import *
 def create_database_schema():
     """Function to create the database file and schema."""
 
-    with sqlite3.connect(DATABASE_FILE) as conn:
-        
-        cur = conn.cursor()
+    try:
+        with sqlite3.connect(DATABASE_FILE) as conn:
+            
+            cur = conn.cursor()
 
-        # Drop the tables if they exist
-        cur.execute("DROP TABLE IF EXISTS heroes_powers")
-        cur.execute("DROP TABLE IF EXISTS heroes")
-        cur.execute("DROP TABLE IF EXISTS powers")
+            # Drop the tables if they exist
+            cur.execute("DROP TABLE IF EXISTS heroes_powers")
+            cur.execute("DROP TABLE IF EXISTS heroes")
+            cur.execute("DROP TABLE IF EXISTS powers")
 
-        # create the heroes table in the database
-        cur.execute("""
-            CREATE TABLE heroes (
-            hero_id INTEGER NOT NULL
-            , hero_name TEXT NOT NULL
-            , gender TEXT
-            , eye_color TEXT
-            , species TEXT
-            , hair_color TEXT
-            , height REAL
-            , weight REAL
-            , publisher TEXT
-            , skin_color TEXT
-            , alignment TEXT
-            , PRIMARY KEY (hero_id)
-            );
-            """)
+            # create the heroes table in the database
+            cur.execute("""
+                CREATE TABLE heroes (
+                hero_id INTEGER NOT NULL
+                , hero_name TEXT NOT NULL
+                , gender TEXT
+                , eye_color TEXT
+                , species TEXT
+                , hair_color TEXT
+                , height REAL
+                , weight REAL
+                , publisher TEXT
+                , skin_color TEXT
+                , alignment TEXT
+                , PRIMARY KEY (hero_id)
+                );
+                """)
 
-        # create the powers table in the database
-        cur.execute("""
-            CREATE TABLE powers (
-            power_id INTEGER NOT NULL
-            , power_name TEXT
-            , power_type TEXT
-            , power_level INTEGER
-            , PRIMARY KEY (power_id)
-            );
-            """)
+            # create the powers table in the database
+            cur.execute("""
+                CREATE TABLE powers (
+                power_id INTEGER NOT NULL
+                , power_name TEXT
+                , power_type TEXT
+                , power_level INTEGER
+                , PRIMARY KEY (power_id)
+                );
+                """)
 
-        # create the heroes_powers table in the database
-        cur.execute("""
-            CREATE TABLE heroes_powers (
-            hero_id INTEGER NOT NULL
-            , power_id INTEGER NOT NULL
-            , PRIMARY KEY (hero_id, power_id)
-            , FOREIGN KEY (hero_id) REFERENCES heroes (hero_id)
-            , FOREIGN KEY (power_id) REFERENCES powers (power_id)
-            );
-            """)
-        
-        # commit the changes
-        conn.commit()
+            # create the heroes_powers table in the database
+            cur.execute("""
+                CREATE TABLE heroes_powers (
+                hero_id INTEGER NOT NULL
+                , power_id INTEGER NOT NULL
+                , PRIMARY KEY (hero_id, power_id)
+                , FOREIGN KEY (hero_id) REFERENCES heroes (hero_id)
+                , FOREIGN KEY (power_id) REFERENCES powers (power_id)
+                );
+                """)
+            
+            # commit the changes
+            conn.commit()
+    
+    # Raise any exceptions
+    except Exception as e:
+        print("Error creating database schema: {}".format(e))
+        raise e
 
 def load_data():
     """Function to load data into the database."""
 
-    with sqlite3.connect(DATABASE_FILE) as conn:
+    if not HEROES_FILENAME.is_file() or not POWERS_FILENAME.is_file() or not HEROES_POWERS_FILENAME.is_file():
+        raise Exception("One or more of the data files does not exist at the expected location.")
+    
+    try:
+        with sqlite3.connect(DATABASE_FILE) as conn:
 
-        cur = conn.cursor()
+            cur = conn.cursor()
 
-        # delete data from the tables if they exist
-        cur.execute("DELETE FROM heroes_powers")
-        cur.execute("DELETE FROM heroes")
-        cur.execute("DELETE FROM powers")
+            # delete data from the tables if they exist
+            cur.execute("DELETE FROM heroes_powers")
+            cur.execute("DELETE FROM heroes")
+            cur.execute("DELETE FROM powers")
 
-        # load data into the heroes table
-        with open(HEROES_FILENAME, 'r') as file:
-            # Skip the header row
-            next(file)
+            # load data into the heroes table
+            with open(HEROES_FILENAME, 'r') as file:
+                # Skip the header row
+                next(file)
 
-            for line in file:
-                # Split each line into values and insert into the table
-                row = line.strip().split(',')
-                cur.execute("INSERT INTO heroes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (int(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
-        
-        # load the data into the powers table
-        with open(POWERS_FILENAME, 'r') as file:
-            # Skip the header row
-            next(file)
+                for line in file:
+                    # Split each line into values and insert into the table
+                    row = line.strip().split(',')
+                    cur.execute("INSERT INTO heroes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (int(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10]))
+            
+            # load the data into the powers table
+            with open(POWERS_FILENAME, 'r') as file:
+                # Skip the header row
+                next(file)
 
-            for line in file:
-                # Split each line into values and insert into the table
-                row = line.strip().split(',')
-                cur.execute("INSERT INTO powers VALUES (?, ?, ?, ?)", (int(row[0]), row[1], row[2], int(row[3])))
-        
-        # load the data into the heroes_powers table
-        with open(HEROES_POWERS_FILENAME, 'r') as file:
-            # Skip the header row
-            next(file)
+                for line in file:
+                    # Split each line into values and insert into the table
+                    row = line.strip().split(',')
+                    cur.execute("INSERT INTO powers VALUES (?, ?, ?, ?)", (int(row[0]), row[1], row[2], int(row[3])))
+            
+            # load the data into the heroes_powers table
+            with open(HEROES_POWERS_FILENAME, 'r') as file:
+                # Skip the header row
+                next(file)
 
-            for line in file:
-                # Split each line into values and insert into the table
-                row = line.strip().split(',')
-                cur.execute("INSERT INTO heroes_powers VALUES (?, ?)", (int(row[0]), int(row[1])))
-
+                for line in file:
+                    # Split each line into values and insert into the table
+                    row = line.strip().split(',')
+                    cur.execute("INSERT INTO heroes_powers VALUES (?, ?)", (int(row[0]), int(row[1])))
+    
+    # Raise any exceptions
+    except Exception as e:
+        print("Error loading data: {}".format(e))
+        raise e
 
 create_database_schema()
 load_data()
